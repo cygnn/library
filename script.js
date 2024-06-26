f1f358333899461d8d4277d6352813f082295bb3
Clyde <113117498+cygnn@users.noreply.github.com>
2024-06-26 14:28:23 +0800

    used class for object

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 const myLibrary = [];
 
-function Book(title, author, pages, isRead) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.isRead = Boolean(isRead);
+class Book{
+    constructor(title,author,pages,isRead){
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.isRead = Boolean(isRead);
+    }
 }
+
+// function Book(title, author, pages, isRead) {
+//     this.title = title;
+//     this.author = author;
+//     this.pages = pages;
+//     this.isRead = Boolean(isRead);
+// }
 
 function addBookToLibrary(title, author, pages, isRead) {
     let newBook = new Book(title, author, pages, isRead);
